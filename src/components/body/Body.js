7d341159d4390e6608e65fb2ecbeb914c6ec4e25
Zealll <eriznis@gmail.com>
2020-03-31 13:52:44 -0400

    Small updates

--- src/components/body/Body.js
+++ src/components/body/Body.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState } from 'react'
 import sun from '../../resources/sun.png'
 import topLeft from '../../resources/top-left.png'
 import topRight from '../../resources/top-right.png'
@@ -10,9 +10,11 @@ import maze from '../../resources/game.PNG'
 import revLogo from '../../resources/Favicon.png'
 import mazeLogo from '../../resources/maze-logo.ico'
 import fullDiscovery from '../../resources/full-dicovery.png'
+import skills from '../../resources/skills.png'
 
 
 const Body = () => {
+    const [clicked, setClicked] = useState(false)
 
     const copyToClip = () => {
         const tempInput = document.createElement('input')
@@ -21,6 +23,14 @@ const Body = () => {
         tempInput.select()
         document.execCommand('copy')
         document.body.removeChild(tempInput)
+    }
+
+    const toggleClicked = () => {
+        setClicked(true)
+
+        setTimeout(() => {
+            setClicked(false)
+        }, 5000)
     }
 
     return (
@@ -86,18 +96,25 @@ const Body = () => {
                     </div>
                 </div>
             </div>
+            
+            <div className='skills'>
+                <img src={skills} alt='Skill list'/>
+            </div>
 
             <div className='contact' id='contact'>
                 <div className='text'>
                     <h4>Want to Find Out More?</h4>
                     <h2>Let’s connect!</h2>
-                    <p>Hit me up to hire, collaborate, or talk about code. I’m always wiiling to</p>
+                    <p>Hit me up to hire, collaborate, or talk about code. I’m always wiling to</p>
                     <p className='top'>meet new people and network.</p>
                 </div>
                 <div className='links'>
                     <a href='https://www.linkedin.com/in/elan-riznis/' target='_blank' rel='noopener noreferrer'><svg stroke="currentColor" fill="currentColor" stroke-width="0" viewBox="0 0 1024 1024" height="2.5em" width="2.5em" xmlns="http://www.w3.org/2000/svg"><path d="M880 112H144c-17.7 0-32 14.3-32 32v736c0 17.7 14.3 32 32 32h736c17.7 0 32-14.3 32-32V144c0-17.7-14.3-32-32-32zM349.3 793.7H230.6V411.9h118.7v381.8zm-59.3-434a68.8 68.8 0 1 1 68.8-68.8c-.1 38-30.9 68.8-68.8 68.8zm503.7 434H675.1V608c0-44.3-.8-101.2-61.7-101.2-61.7 0-71.2 48.2-71.2 98v188.9H423.7V411.9h113.8v52.2h1.6c15.8-30 54.5-61.7 112.3-61.7 120.2 0 142.3 79.1 142.3 181.9v209.4z"></path></svg></a>
                     <a href='https://github.com/Zealll' target='_blank' rel='noopener noreferrer'><svg stroke="currentColor" fill="currentColor" stroke-width="0" viewBox="0 0 16 16" height="2em" width="2em" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M8 0C3.58 0 0 3.58 0 8c0 3.54 2.29 6.53 5.47 7.59.4.07.55-.17.55-.38 0-.19-.01-.82-.01-1.49-2.01.37-2.53-.49-2.69-.94-.09-.23-.48-.94-.82-1.13-.28-.15-.68-.52-.01-.53.63-.01 1.08.58 1.23.82.72 1.21 1.87.87 2.33.66.07-.52.28-.87.51-1.07-1.78-.2-3.64-.89-3.64-3.95 0-.87.31-1.59.82-2.15-.08-.2-.36-1.02.08-2.12 0 0 .67-.21 2.2.82.64-.18 1.32-.27 2-.27.68 0 1.36.09 2 .27 1.53-1.04 2.2-.82 2.2-.82.44 1.1.16 1.92.08 2.12.51.56.82 1.27.82 2.15 0 3.07-1.87 3.75-3.65 3.95.29.25.54.73.54 1.48 0 1.07-.01 1.93-.01 2.2 0 .21.15.46.55.38A8.013 8.013 0 0 0 16 8c0-4.42-3.58-8-8-8z"></path></svg></a>
-                    <svg onClick={copyToClip} name='eriznis@gmail.com' className='email' stroke="currentColor" fill="currentColor" stroke-width="0" viewBox="0 0 1024 1024" height="2.5em" width="2.5em" xmlns="http://www.w3.org/2000/svg"><path d="M928 160H96c-17.7 0-32 14.3-32 32v640c0 17.7 14.3 32 32 32h832c17.7 0 32-14.3 32-32V192c0-17.7-14.3-32-32-32zm-40 110.8V792H136V270.8l-27.6-21.5 39.3-50.5 42.8 33.3h643.1l42.8-33.3 39.3 50.5-27.7 21.5zM833.6 232L512 482 190.4 232l-42.8-33.3-39.3 50.5 27.6 21.5 341.6 265.6a55.99 55.99 0 0 0 68.7 0L888 270.8l27.6-21.5-39.3-50.5-42.7 33.2z"></path></svg>
+                    <div>
+                        <p className={!clicked ? 'none' : 'copied'}>Copied!</p>
+                        <svg onClick={() => {copyToClip(); toggleClicked()}} name='eriznis@gmail.com' className={!clicked ? 'email' : 'email clicked'} stroke="currentColor" fill="currentColor" stroke-width="0" viewBox="0 0 1024 1024" height="2.5em" width="2.5em" xmlns="http://www.w3.org/2000/svg"><path d="M928 160H96c-17.7 0-32 14.3-32 32v640c0 17.7 14.3 32 32 32h832c17.7 0 32-14.3 32-32V192c0-17.7-14.3-32-32-32zm-40 110.8V792H136V270.8l-27.6-21.5 39.3-50.5 42.8 33.3h643.1l42.8-33.3 39.3 50.5-27.7 21.5zM833.6 232L512 482 190.4 232l-42.8-33.3-39.3 50.5 27.6 21.5 341.6 265.6a55.99 55.99 0 0 0 68.7 0L888 270.8l27.6-21.5-39.3-50.5-42.7 33.2z"></path></svg>
+                    </div>
                 </div>
             </div>
         </div>
